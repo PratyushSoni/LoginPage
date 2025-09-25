998b9c530d4abffb00d407647daabda6b9cb4ba1
PratyushSoni <pratyushsoni2003@gmail.com>
2025-09-25 15:20:30 +0530

    made changes to launchsettings and web.config

---
+++ LoginPageWebApp/Helpers/LanguageHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace LoginPageWebApp.Helpers
+{
+    public static class LanguageHelper
+    {
+        private static readonly string xmlPath = HttpContext.Current.Server.MapPath("~/App_Data/Languages.xml");
+
+        public static string GetText(string key)
+        {
+            string lang = "en"; // default
+            if (HttpContext.Current.Request.Cookies["CurrentLanguage"] != null)
+                lang = HttpContext.Current.Request.Cookies["CurrentLanguage"].Value;
+
+            XDocument doc = XDocument.Load(xmlPath);
+            var element = doc.Descendants("Text")
+                             .FirstOrDefault(x => x.Attribute("Key").Value == key);
+
+            if (element != null)
+            {
+                var value = element.Element(lang);
+                if (value != null)
+                    return value.Value;
+            }
+
+            return key; // fallback if key not found
+        }
+    }
+}
